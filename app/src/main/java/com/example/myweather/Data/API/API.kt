ac9996dec7513add82ce5131e8e74a1a0bb52c8e
lord_bc <wolf.1992@list.ru>
2020-07-28 19:12:01 +0300

    ...

--- app/src/main/java/com/example/myweather/Data/API/API.kt
+++ app/src/main/java/com/example/myweather/Data/API/API.kt
@@ -8,15 +8,8 @@ import retrofit2.http.Query
 
 interface API {
 
-    @GET("weather?")
-    fun getWeather(@Query("q") cityname : String, @Query("units=metric&appid=") api : String) : Call<WeatherClass>
-
     @GET("weather")
     fun getWeather2(@Query("q") cityname : String, @Query("units") metric : String, @Query("appid") api : String) : Call<WeatherClass>
-
-    @GET("weather?q={test}&units=metric&appid={appid}")
-    fun getWeather3(@Path("test") cityname : String, @Path("appid") api : String) : Call<WeatherClass>
-
-
+    
 
 }
